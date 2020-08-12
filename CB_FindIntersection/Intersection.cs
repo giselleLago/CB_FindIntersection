8b15c2c7037ad5f52e3b03f258ac06d0c14640ac
giselleLago <gisellelago25994@gmail.com>
2020-08-12 12:30:28 +0200

    Add project files.

---
+++ CB_FindIntersection/Intersection.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CB_FindIntersection
+{
+    public class Intersection
+    {
+        public string FindIntersection(string[] strArr)
+        {
+            var firstList = strArr[0].Split(", ").ToList();
+            var secondList = strArr[1].Split(", ").ToList();
+            var result = new List<int>();
+
+            foreach (var i in firstList)
+            {
+                var iToInt = int.Parse(i);
+                foreach (var j in secondList)
+                {
+                    var jToInt = int.Parse(j);
+                    if (iToInt == jToInt)
+                    {
+                        result.Add(iToInt);
+                        break;
+                    }
+                }
+            }
+            result.Sort();
+            return string.Join(", ", result);
+
+        }
+    }
+}
---
+++ CB_FindIntersection/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace CB_FindIntersection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var a = new Intersection();
+            Console.WriteLine(a.FindIntersection(new string[] { "1, 3, 9, 10, 17, 18", "1, 4, 9, 10" }));
+            Console.ReadKey();
+        }
+    }
+}
